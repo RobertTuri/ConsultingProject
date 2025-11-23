f834cf282564ce45181569a5ab8d6aecf9a59156
RobertTuri <RobertTheodorTuri@gmail.com>
2025-11-23 18:02:52 +0000

    Adding dashboard file

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,7 @@
-# For creating the dashboard later (likely using Streamlit) -> Remember to add to requirements.txt if needed
+import streamlit as st
+import pandas as pd
+from stock_chart import fetch_stock_data
+from sentiment_vader_dashboard import analyse_sentiment_v
+from sentiment_transformer_dashboard import analyse_sentiment_t
+
+st.title("News & Stock Sentiment Dashboard")
